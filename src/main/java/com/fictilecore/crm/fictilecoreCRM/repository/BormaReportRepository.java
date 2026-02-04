8d14b61d8959b25cf3b0747e091276beda8048f8
santoshkarpurashetti <karpurashettis@gmail.com>
2026-02-04 11:09:46 +0530

    04-02-2026-3

--- src/main/java/com/fictilecore/crm/fictilecoreCRM/repository/BormaReportRepository.java
+++ src/main/java/com/fictilecore/crm/fictilecoreCRM/repository/BormaReportRepository.java
@@ -16,55 +16,7 @@ import com.fictilecore.crm.fictilecoreCRM.entity.Tenant;
 
 @Repository
 public interface BormaReportRepository extends JpaRepository<BormaReport, Long>  {
-       List<BormaReport> findByDate(LocalDate date);
 
-    List<BormaReport> findByEmployee_Id(Long employeeId);
-
-   List<BormaReport> findByTenant_Id(Long tenantId);
-
-   List<BormaReport> findByTenant_IdAndEmployee_Id(Long tenantId, Long employeeId);
-
-   List<BormaReport> findByTenant_IdAndDate(Long tenantId, LocalDate date);
-
-      // Get all reports except completed for a tenant
-    @Query("SELECT r FROM BormaReport r WHERE r.tenant.id = :tenantId AND (r.status IS NULL OR r.status <> 'Completed')")
-    List<BormaReport> findAllExceptCompletedByTenant(@Param("tenantId") Long tenantId);
-
-    
-@Query("""
-SELECT new com.fictilecore.crm.fictilecoreCRM.dto.BormaReportResponse(
-    c.id,
-    c.date,
-    c.lotMark,
-    c.origin,
-    c.sizeRange,
-    c.afterBormaKernelMoisture,
-    c.BormaTimeDuration,
-    c.BormaTemperature,
-    c.aftrBormaWholes,
-    c.aftrBormaBrokens,
-    c.shortWholes,
-    c.shortBrokens,
-    c.totalWholes,
-    c.totalShort,
-    null,
-    e.employee_name
-)
-FROM BormaReport c
-LEFT JOIN c.employee e
-WHERE (:tenantId IS NULL OR c.tenant.id = :tenantId)
-  AND (:employeeId IS NULL OR e.id = :employeeId)
-  AND (:date IS NULL OR c.date = :date)
-ORDER BY c.date DESC
-""")
-
-List<BormaReportResponse> findReportsByTenantEmployeeAndDate(
-    @Param("tenantId") Long tenantId,
-    @Param("employeeId") Long employeeId,
-    @Param("date") LocalDate date
-);
-
-  Optional<BormaReport> findByIdAndTenant_Id(Long reportId, Long tenantId);
 
 
 }
